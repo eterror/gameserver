d69ca58a2a38a331528d66845767b9ae219aabbf
terror <solargrim@gmail.com>
2026-09-21 23:33:15 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const WebSocket = require('ws');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('OK');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT);
